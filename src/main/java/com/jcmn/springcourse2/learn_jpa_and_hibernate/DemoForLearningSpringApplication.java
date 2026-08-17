92fe2e7ff688cda5bc3146abbf277b4feb3db471
JuanMartinN02 <juannogueiram@gmail.com>
2026-08-17 12:45:56 -0400

    Initial commit

---
+++ src/main/java/com/jcmn/springcourse2/learn_jpa_and_hibernate/DemoForLearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jcmn.springcourse2.learn_jpa_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForLearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForLearningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcmn/springcourse2/learn_jpa_and_hibernate/DemoForLearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcmn.springcourse2.learn_jpa_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForLearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
